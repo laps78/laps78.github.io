bcacf61664f6ab5f40e85966f7c6c749e9569030
Evgeniy Laptev <79728932+laps78@users.noreply.github.com>
2022-02-16 14:30:24 +0300

    html layout desktop margins added

--- worldWideClock/js/wwc.js
+++ worldWideClock/js/wwc.js
@@ -4,16 +4,16 @@ function displayCanvas(){
   var contextHTML = canvasHTML.getContext("2d");
   contextHTML.strokeRect(0, 0, canvasHTML.width, canvasHTML.height);
 
-  //Расчет координат центра и радиуса часов
+  //Calculating clocktable centre & size
   var radiusClock = canvasHTML.width / 2 - 10;
   var xCenterClock = canvasHTML.width / 2;
   var yCenterClock = canvasHTML.height / 2;
 
-  //Очистка экрана. 
+  //Clear screen 
   contextHTML.fillStyle = "#ffffff";
   contextHTML.fillRect(0, 0, canvasHTML.width, canvasHTML.height);
 
-  //Рисуем контур часов
+  //Drawing clock contour
   contextHTML.strokeStyle = "#000000";
   contextHTML.lineWidth = 1;
   contextHTML.beginPath();
@@ -22,12 +22,12 @@ function displayCanvas(){
   contextHTML.stroke();
   contextHTML.closePath();
 
-  //Рисуем рисочки часов
-  var radiusNum = radiusClock - 10; //Радиус расположения рисочек	
+  //Drawing clocktable pins & points
+  var radiusNum = radiusClock - 10;	
   var radiusPoint;
   for(var tm = 0; tm < 60; tm++){
   contextHTML.beginPath();
-  if(tm % 5 == 0){radiusPoint = 5;}else{radiusPoint = 2;} //для выделения часовых рисочек
+  if(tm % 5 == 0){radiusPoint = 5;}else{radiusPoint = 2;} //for points selection
   var xPointM = xCenterClock + radiusNum * Math.cos(-6*tm*(Math.PI/180) + Math.PI/2);
   var yPointM = yCenterClock - radiusNum * Math.sin(-6*tm*(Math.PI/180) + Math.PI/2);
   contextHTML.arc(xPointM, yPointM, radiusPoint, 0, 2*Math.PI, true);
@@ -35,7 +35,7 @@ function displayCanvas(){
   contextHTML.closePath();
   } 
 
-  //Оцифровка циферблата часов
+  //Drawing clocktable numbers
   for(var th = 1; th <= 12; th++){
 contextHTML.beginPath();
 contextHTML.font = 'bold 25px sans-serif';
@@ -55,10 +55,11 @@ contextHTML.closePath();
   var lengthSeconds = radiusNum - 10;
   var lengthMinutes = radiusNum - 15;
   var lengthHour = lengthMinutes / 1.5;
-  var d = new Date();                //Получаем экземпляр даты
-  var t_sec = 6 * d.getSeconds();                           //Определяем угол для секунд
-  var t_min = 6 * (d.getMinutes() + (1/60) * d.getSeconds()); //Определяем угол для минут
-  var t_hour = 30 * (d.getHours() + (1/60) * d.getMinutes()); //Определяем угол для часов
+  var d = new Date();
+  //Calculating arrow corner clockwise
+  var t_sec = 6 * d.getSeconds();
+  var t_min = 6 * (d.getMinutes() + (1/60) * d.getSeconds());
+  var t_hour = 30 * (d.getHours() + (1/60) * d.getMinutes());
 
   //Drawing seconds arrow
   contextHTML.beginPath();
@@ -75,7 +76,7 @@ contextHTML.closePath();
   contextHTML.lineWidth = 3;
   contextHTML.moveTo(xCenterClock, yCenterClock);
   contextHTML.lineTo(xCenterClock + lengthMinutes*Math.cos(Math.PI/2 - t_min * (Math.PI/180)),
-       yCenterClock - lengthMinutes * Math.sin(Math.PI/2 - t_min * (Math.PI/180)));
+    yCenterClock - lengthMinutes * Math.sin(Math.PI/2 - t_min * (Math.PI/180)));
   contextHTML.stroke();
   contextHTML.closePath();
 
@@ -84,7 +85,7 @@ contextHTML.closePath();
   contextHTML.lineWidth = 5;
   contextHTML.moveTo(xCenterClock, yCenterClock);
   contextHTML.lineTo(xCenterClock + lengthHour * Math.cos(Math.PI/2 - t_hour*(Math.PI/180)),
-       yCenterClock - lengthHour * Math.sin(Math.PI/2 - t_hour * (Math.PI/180)));
+    yCenterClock - lengthHour * Math.sin(Math.PI/2 - t_hour * (Math.PI/180)));
   contextHTML.stroke();
   contextHTML.closePath();	
 
