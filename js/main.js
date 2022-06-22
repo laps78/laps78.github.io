a64042af215d3919e69b2323b852624b402f9b13
laps78 <9254384@mail.ru>
2022-06-22 13:02:22 +0300

    web developer portfolio: footer contacts & socials added

--- js/main.js
+++ js/main.js
@@ -1,28 +1,3 @@
-window.onload = function userApi() {
+window.onload = () => {
   
-  //popups
-  const linksPopupOpen = document.querySelectorAll('.link_popup_open');
-  const popup = document.querySelectorAll('.popup__wrapper');
-  const popupCloseButtons = document.querySelectorAll('popup_close_btn');
-
-  function popupClose(popup) {
-    popup.classList.toggle('visually-hidden');
-  }
-  //close popup from missclick
-  document.addEventListener('click', (e) => {
-    if (e.target === popup) {
-      popupClose(e.target);
-    };
-  });
-  
-  //sertificate popup
- 
-
-  //close popup from button
-  popupCloseButtons.forEach((button) => {
-    button.addEventListener('click', (e) => {
-      e.preventDefault();
-      popup.classList.toggle('visually-hidden');
-    });
-  });  
 };
